a54a11f67b7f2b9ca3de6f57b605c8e4ef8814b5
ErikBoesen <me@erikboesen.com>
2019-09-29 01:05:16 -0400

    Add entry point

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject eightball "0.1.0"
   :description "GroupMe bot for telling the future."
   :url "http://eightballgroupme.herokuapp.com"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
-                 [environ "1.1.0"]
-                 [camel-snake-kebab "0.4.0"]]
+  :main eightball.server
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.3.1"]
+                 [ring/ring-json "0.2.0"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [compojure "1.1.6"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.3.1"]]
+  :plugins [[lein-ring "0.8.10"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "eightball-standalone.jar"
   :profiles {:production {:env {:production true}}})
