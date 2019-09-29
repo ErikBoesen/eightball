7af85126bd6d3358220c1afcda726ba07020d900
ErikBoesen <me@erikboesen.com>
2019-09-29 00:17:07 -0400

    Depend on camel-snake-kebab

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [camel-snake-kebab "0.4.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
